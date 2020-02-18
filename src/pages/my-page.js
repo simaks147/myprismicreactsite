1b6414b497f5871618174587a0a04a29f8a06247
Maksim Lobarev <lobarev@galaxy-labs.ru>
2020-02-18 18:18:34 +0300

    my new page

---
+++ src/pages/my-page.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Layout from "../components/layouts"
+
+const MyPage = () => (
+    <Layout>
+        <h1>My New Page</h1>
+    </Layout>
+)
+
+export default MyPage
